7a9f11b9b2589f23fd61cbb45c23097d4fd62f69
Gabriele Culotta <culottagabriele@MacBook-Pro-di-Gabriele.local>
2022-03-15 13:23:41 +0100

    feat(general)todo add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [arrTodos, setArrTodos] = useState([]); // array contenente la lista
+  const [todo, setTodo] = useState(""); //
+
+  // Esempio dalla quale ho preso l'idea
+
+  /*  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    };
+    setArrTodos([...arrTodos].concat(newTodo));
+    setTodo("");
+  }  */
+
+  const newTodo1 = (e) => {
+    e.preventDefault();
+
+    const obj = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    };
+
+    setArrTodos([...arrTodos, obj]);
+    setTodo("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo App with React</h1>
+      <form onSubmit={newTodo1}>
+        <input
+          type="text"
+          onChange={(item) => setTodo(item.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
+        {arrTodos.map((todo) => (
+          <p>{todo.text}</p>
+        ))}
+      </form>
     </div>
   );
 }
